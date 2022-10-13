65af10fed9b49bf758cdfa0d623ef6ae44fd69c2
stumburs <stumburs@inbox.lv>
2022-10-13 20:12:24 +0300

    Navbar update.

--- templates/navbar.js
+++ templates/navbar.js
@@ -5,9 +5,30 @@ class Navbar extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-        <div class="test">
-            <img src="https://assets.vg247.com/current//2014/07/cj.jpg" />
-        </div>
+        <ul class="nav nav-pills justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link" href="/index.html">Home</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Button 1</a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">Dropdown maybe</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Link</a></li>
+                    <li><a class="dropdown-item" href="#">Link 2</a></li>
+                    <div class="dropdown-divider"></div>
+                    <li><a class="dropdown-item">Link 3</a></li>
+                    <li><a class="dropdown-item">Link 4</a></li>
+                    <li><a class="dropdown-item">Link 5</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+        </ul>
       `;
     }
 }

8fe6d4c5387b761e27a332491e802d9f851e9531
stumburs <stumburs@inbox.lv>
2022-10-27 18:10:06 +0300

    Profile page cookies.

---
+++ js/cookies.js
@@ -0,0 +1,22 @@
+function getCookie(cookieName) {
+    let cookie = {};
+    document.cookie.split(";").forEach(function (el) {
+        let [key, value] = el.split("=");
+        cookie[key.trim()] = value;
+    });
+    return cookie[cookieName];
+}
+
+function changeBonusPoints(cookieName, amount) {
+    let previousValue = getCookie(cookieName);
+    document.cookie = `bonuspoints=${Number(previousValue) + amount}`;
+}
+
+function changeCookieValue(cookieName, value) {
+    document.cookie = `${cookieName}=${value}`;
+}
+
+function readCookies() {
+    document.getElementById("cookie-username").innerHTML += getCookie("username");
+    document.getElementById("cookie-bonuspoints").innerHTML += getCookie("bonuspoints");
+}

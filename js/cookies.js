2754231aeed3502d3f0fb3877731c67b3636993f
stumburs <stumburs@inbox.lv>
2022-10-29 03:03:21 +0300

    Added reset cookies debug button.

--- js/cookies.js
+++ js/cookies.js
@@ -22,3 +22,24 @@ function readCookies() {
     document.getElementById("cookie-bonuspointsgained").innerHTML += getCookie("bonuspointsgained");
     document.getElementById("cookie-bonuspointslost").innerHTML += getCookie("bonuspointslost");
 }
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+function debugResetCookies() {
+    deleteAllCookies();
+    let username = prompt("Enter your username");
+    document.cookie = `username=${username}`;
+    document.cookie = "bonuspoints=1000";
+    document.cookie = "bonuspointsgained=1000";
+    document.cookie = "bonuspointslost=0";
+    location.reload();
+}
